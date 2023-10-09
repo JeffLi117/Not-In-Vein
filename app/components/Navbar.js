f7b23f615ee0ee1ecb50f5cad4aeb84ccb6586f0
JeffLi117 <li.jeffreyt@yahoo.com>
2023-10-09 16:13:59 -0500

    Revise navbar to not change pixels when hovering over buttons; change color, font styles, add logo

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
 import { UserAuth } from "../context/AuthContext";
-import { Nunito, Alegreya } from 'next/font/google';
+import { Nunito, Alegreya, Bebas_Neue } from 'next/font/google';
 import { FiMenu } from 'react-icons/fi';
-import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai';
 import Link from 'next/link';
+import Not_In_Vein_logo from "../../public/Not_In_Vein_logo.png";
+import Image from "next/image";
  
 const nunito = Nunito({
   subsets: ['latin'],
@@ -13,9 +15,9 @@ const nunito = Nunito({
   style: 'normal'
 })
 
-const alegreya = Alegreya({
+const bebas_n = Bebas_Neue({
   subsets: ['latin'],
-  variable: '--font-alegreya',
+  variable: '--font-bebas_n',
   weight: '400',
   style: 'normal'
 })
@@ -50,8 +52,19 @@ export default function Navbar() {
     }, [user])
 
     return (
-        <nav className="md:flex items-center justify-between p-2">
-            <Link href="/" className={`text-4xl ${alegreya.className}`}>Not In Vein</Link>
+        <nav className="md:flex items-center justify-between p-2 bg-red-500">
+            <Link href="/" className={`text-4xl ${bebas_n.className} flex items-center justify-center gap-2`}>
+                <Image 
+                    className="rounded-lg"
+                    src={Not_In_Vein_logo}
+                    width={50}
+                    height={50}
+                    alt="PNG of Not In Vein logo"
+                />
+                <div className="flex items-center justify-center">
+                    Not In Vein
+                </div>
+            </Link>
             {/* menu icon */}
             <div onClick={() => setOpen(!open)} className="absolute top-4 right-6 cursor-pointer md:hidden">
                 {open? 
@@ -62,15 +75,15 @@ export default function Navbar() {
             </div>
             {loading ? null : !user ? 
                 (<div className={`md:flex md:items-center md:justify-end md:gap-4  ${open? "block":"hidden"}`}>
-                    <button onClick={handleSignIn} className="block md:inline- w-full md:w-fit text-left md:text-center py-1 px-2 rounded-md  hover:text-red-400 hover:border-red-400 hover:border">Login</button>
-                    <button onClick={handleSignIn} className="block md:inline-block w-full md:w-fit  text-left md:text-center py-1 px-2 rounded-md  hover:text-red-400 hover:border-red-400 hover:border">Sign Up</button>
+                    <button onClick={handleSignIn} className="block md:inline- w-full md:w-fit text-left md:text-center py-1 px-2 rounded-md hover:text-white hover:border-white border border-transparent">Login</button>
+                    <button onClick={handleSignIn} className="block md:inline-block w-full md:w-fit text-left md:text-center py-1 px-2 rounded-md hover:text-white hover:border-white border border-transparent">Sign Up</button>
                 </div>)
             :
                 (<div className={`md:flex md:items-center md:justify-end md:gap-4  ${open? "block":"hidden"}`}>
                     <div className="py-1 px-2">Hi, {user.displayName}</div>
                     <Link href="/donate" className="block py-1 px-2 rounded-md hover:bg-red-400 hover:text-white">Donate</Link>
-                    <Link href="/profile" className="block py-1 px-2 rounded-md  hover:text-red-400 hover:border-red-400 hover:border">Your Profile</Link>
-                    <Link href="/" onClick={handleSignOut} className="block py-1 px-2 rounded-md hover:text-red-400 hover:border-red-400 hover:border">Sign Out</Link>
+                    <Link href="/profile" className="block py-1 px-2 rounded-md hover:text-white hover:border-white border border-transparent">Your Profile</Link>
+                    <Link href="/" onClick={handleSignOut} className="block py-1 px-2 rounded-md hover:text-white hover:border-white border border-transparent">Sign Out</Link>
                 </div>) 
             }
         </nav>
