b26550b68337ab8b2fae786518270e4bcd226bae
JeffLi117 <li.jeffreyt@yahoo.com>
2023-11-16 23:11:15 -0600

    Implement new API for mapping and searching within a specific zip code; will display a pop-up window when markers are clicked that can also give directions to that specific location

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,19 +1,37 @@
 "use client"
 import 'dotenv/config';
 import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-const MapComponent = () => {
-    return <Map
-                mapLib={import('mapbox-gl')}
-                initialViewState={{
-                longitude: -100,
-                latitude: 40,
-                zoom: 3.5
-                }}
-                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-                style={{width: 400, height: 400}}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            />;
+const MapComponent = ({ userLat, userLong }) => {
+    
+    return <div>
+            {userLat && userLong ? 
+                <Map
+                    mapLib={import('mapbox-gl')}
+                    initialViewState={{
+                        longitude: userLong,
+                        latitude: userLat,
+                        zoom: 1
+                    }}
+                    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+                    style={{width: 400, height: 400}}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                /> : 
+                <Map
+                    mapLib={import('mapbox-gl')}
+                    initialViewState={{
+                        longitude: -100,
+                        latitude: 40,
+                        zoom: 3.5
+                    }}
+                    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+                    style={{width: 400, height: 400}}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                />
+            }
+            </div> 
+    
 }
 
 export default MapComponent
