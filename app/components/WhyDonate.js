295831358df9bfe0650129eddee6d8d94dcfb105
JeffLi117 <li.jeffreyt@yahoo.com>
2023-12-13 14:08:55 -0600

    Modify About page to include icons within reasonsToDonate array and change mobile view

--- app/components/WhyDonate.js
+++ app/components/WhyDonate.js
@@ -1,6 +1,8 @@
 "use client"
 import React, { useState } from 'react';
 import { Alegreya } from 'next/font/google';
+import { FaHeart, FaFireAlt, FaClipboardCheck } from "react-icons/fa";
+import { MdHealthAndSafety } from "react-icons/md";
 
 const alegreya = Alegreya({
   subsets: ['latin'],
@@ -10,10 +12,10 @@ const alegreya = Alegreya({
 })
 
 const reasonsToDonate = [
-    {id: 1, string: "Improve/save the lives of 3 people.", icon: null},
-    {id: 2, string: "Get a free health screening.", icon: null},
-    {id: 3, string: "Lower your blood pressure and heart attack risk.", icon: null},
-    {id: 4, string: "Burn 500 calories without breaking a sweat.", icon: null},
+    {id: 1, string: "Improve/save the lives of 3 people.", icon: <FaHeart/>},
+    {id: 2, string: "Get a free health screening.", icon: <FaClipboardCheck/>},
+    {id: 3, string: "Lower your blood pressure and heart attack risk.", icon: <MdHealthAndSafety/>},
+    {id: 4, string: "Burn 500 calories without breaking a sweat.", icon: <FaFireAlt/>},
 ]
 
 export default function WhyDonate() {
@@ -23,14 +25,14 @@ export default function WhyDonate() {
     setIsOpen(!isOpen)
   }
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
-        <button onClick={() => toggleWhy()} className={`${alegreya.className} border border-transparent w-[120px] rounded-md p-2 bg-red-400 text-white hover:border-black hover:text-black`}>
+    <div className="flex flex-col justify-center items-center gap-4 mb-2 md:mb-0">
+        <button onClick={() => toggleWhy()} className={`${alegreya.className} border border-transparent w-[200px] rounded-md p-2 bg-red-400 text-white hover:border-black hover:text-black`}>
             {isOpen ? "Close" : "Why donate?"}
         </button>
         {isOpen && 
-            <ul className="text-left">
+            <ul className="text-left text-sm md:text-base">
                 {reasonsToDonate.map((reason) => {
-                    return <li key={reason.id}>{reason.string}</li>
+                  return <li key={reason.id} className="flex flex-row justify-start items-center gap-1 mt-1">{reason.icon} {reason.string}</li>
             })}
         </ul>}
     </div>
